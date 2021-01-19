9e84df3add06ad09394d6d43a276602765d4bce5
Ertuğrul Baran <ert.baran@gmail.com>
2021-01-20 01:31:44 +0300

    Son Hal

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    // Musteri ekleme, listeleme,silme gibi metotları simule ediniz.
+    class MusteriManager
+    {
+        public static void MusteriEkle(string tcNo, string ad, string soyad, Musteri[] musteriler)
+        {
+            Musteri musteri = new Musteri();
+            for (int i = 0; i < musteriler.Length; i++)
+            {
+                if (musteriler[i] == null)
+                {
+                    musteriler[i] = musteri;
+                    musteri.TC = tcNo;
+                    musteri.Ad = ad;
+                    musteri.Soyad = soyad;
+                    Console.WriteLine("11121314151 | " + musteri.Ad + " " + musteri.Soyad + " kişisi eklendi. ");
+                    break;
+                }
+            }
+        }
+
+        public static void MusteriListele(Musteri[] musteriler)
+        {
+            Console.WriteLine("_____________________________________\n" +
+                "Güncel Müşteri Listesi:\n");
+            foreach (var musteri in musteriler)
+            {
+                if (musteri != null)
+                {
+                    Console.WriteLine(musteri.TC + " " + musteri.Ad + " " + musteri.Soyad);
+                }
+            }
+            Console.WriteLine("_____________________________________");
+        }
+
+        public static void MusteriSil(Musteri[] musteriler, string tcNo)
+        {
+            for (int i = 0; i < musteriler.Length; i++)
+            {
+                if (musteriler[i] != null && musteriler[i].TC == tcNo)
+                {
+                    Array.Clear(musteriler, i, 1);
+                    Console.WriteLine(tcNo + " TC numaralı müşteri silindi.");
+                    break;
+                }
+                else if (i == musteriler.Length-1 && musteriler[i]==null)
+                {
+                    Console.WriteLine("Müşteri bulunamadı.");
+                }
+            }
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -10,6 +10,17 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
+            Musteri[] musteriler = new Musteri[10];
+            MusteriManager.MusteriEkle("12345678910", "Ertuğrul", "Baran", musteriler);
+            MusteriManager.MusteriEkle("12345678911", "Cahit", "Zarifoğlu", musteriler);
+            MusteriManager.MusteriEkle("12345678912", "Halide Edip", "Adıvar", musteriler);
+            MusteriManager.MusteriListele(musteriler);
+            MusteriManager.MusteriSil(musteriler, "12345678910");
+            MusteriManager.MusteriListele(musteriler);
+            MusteriManager.MusteriEkle("12345678910", "Ertuğrul", "Baran", musteriler);
+            MusteriManager.MusteriListele(musteriler);
+
+            Console.Read();
         }
     }
 }
