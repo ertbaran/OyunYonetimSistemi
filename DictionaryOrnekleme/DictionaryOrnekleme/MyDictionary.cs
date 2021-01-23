759d019917d75fffc40db3d9161e9f029dcab297
Ertuğrul Baran <35369764+ertbaran@users.noreply.github.com>
2021-01-23 18:00:48 +0300

    Dictionary Örnekleme Eklendi

---
+++ DictionaryOrnekleme/DictionaryOrnekleme/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryOrnekleme
+{
+    class MyDictionary<Key, Value>
+    {
+        Key[] keys;
+        Value[] values;
+        public MyDictionary()
+        {
+            keys = new Key[0];
+            values = new Value[0];
+        }
+        public void Add(Key key, Value value)
+        {
+            Key[] tempKeys = keys;
+            Value[] tempValues = values;
+            keys = new Key[keys.Length + 1];
+            values = new Value[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public void WriteItems()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " => " + values[i]);
+            }
+
+        }
+    }
+}
